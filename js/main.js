0b09e6007d0843c8ae52c0a05b80612bfd99a173
chirikchekmen <chekmenev2004@mail.ru>
2022-01-01 20:53:26 +0300

    other examples added

--- js/main.js
+++ js/main.js
@@ -6,16 +6,17 @@ window.onload = () => {
     hamburger.classList.toggle("active");
     header.classList.toggle("active");
   });
-  // Highlight js
-  to_highlight = document.querySelectorAll("code");
-  for (let code = 0; code < to_highlight.length; code++) {
-    hljs.highlightBlock(to_highlight[code]);
-  }
   // Locomotive-scrolling
   const scroll = new LocomotiveScroll({
     el: document.querySelector("[data-scroll-container]"),
     smooth: true,
+    reloadOnContextChange: true,
   });
+  // Highlight js
+  to_highlight = document.querySelectorAll("code");
+  for (let code = 0; code < to_highlight.length; code++) {
+    hljs.highlightElement(to_highlight[code]);
+  }
   // Changing states
   const cursor = document.getElementById("cursor");
   const aura = document.getElementById("aura");
@@ -37,6 +38,27 @@ window.onload = () => {
       cursor.className = "hidden";
       aura.className = "hidden";
     });
+  // Intersection Observer
+  const sections = [
+    ...document.querySelectorAll(".fade-in-animate  h2"),
+    ...document.querySelectorAll(".fade-in-animate .text > *"),
+  ];
+  const observer = new IntersectionObserver(
+    (entries) => {
+      entries.forEach((entry) => {
+        entry.target.classList.toggle("show", entry.isIntersecting);
+        if (entry.isIntersecting) observer.unobserve(entry.target);
+      });
+    },
+    {
+      rootMargin: "-10%",
+    }
+  );
+  sections.forEach((section) => {
+    observer.observe(section);
+  });
   // Copyright
   document.getElementById("copyright").textContent = new Date().getFullYear();
+  scroll.update();
+
 };
