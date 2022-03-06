9615640a668cdde8ca23af8251d8b83aa648bc99
chirikchekmen <chekmenev2004@mail.ru>
2022-03-06 22:13:06 +0100

    robots.txt and sitemap.xml added

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, abort
+from flask import Flask, render_template, abort, send_from_directory, request
 from os import getcwd
 from os.path import isfile
 from os.path import join as join_path
@@ -41,6 +41,12 @@ def examples(example):
     if not isfile(join_path(getcwd(), "templates", "examples", f"{example}.html")):
         abort(404)
     return render_template(f"/examples/{example}.html", title=get_tutorial_title(example))
+
+
+@app.route('/robots.txt')
+@app.route('/sitemap.xml')
+def static_from_root():
+    return send_from_directory(app.static_folder, request.path[1:])
 
 
 if __name__ == "__main__":
